207885af0f6a7ba171b4c32f09b7bd6e0601c234
cedev-1 <121963179+cedev-1@users.noreply.github.com>
2024-05-13 23:56:52 +0200

    v1

--- bot_.py
+++ bot_.py
@@ -2,7 +2,6 @@ import requests
 import os
 import tempfile
 
-# Define the base URL and headers
 base_url = 'https://tres-bien.com'
 headers = {
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
@@ -18,7 +17,6 @@ headers = {
     'Priority': 'u=1, i'
 }
 
-# Define the product data
 product_data = {
     'product': '123554',
     'related_product': '',
@@ -26,27 +24,21 @@ product_data = {
     'super_attribute[135]': '16'
 }
 
-# Create a requests Session object
 session = requests.Session()
 
-# Create a new cookie jar file on each script launch
 with tempfile.NamedTemporaryFile(delete=False) as tmp:
     jar = requests.cookies.RequestsCookieJar()
     session.cookies = jar
 
-    # Add a new cookie to the jar
     jar.set_cookie(requests.cookies.create_cookie('PHPSESSID', value=os.urandom(24).hex()))
 
 try:
-    # Make the POST request to add the product to the cart
     response = session.post(f'{base_url}/checkout/cart/add/uenc/aHR0cHM6Ly90cmVzLWJpZW4uY29tL25pa2Utbm9jdGEtYWlyLXpvb20tZHJpdmUtc3Atd2hpdGU%2C/product/123554/', headers=headers, data=product_data)
 
     if response.status_code == 200:
-        # Make the GET request to load the cart section
         cart_response = session.get(f'{base_url}/customer/section/load', headers=headers)
         if cart_response.status_code == 200:
             print('ok')
-            # Make the GET request to load the checkout page
             checkout_response = session.get(f'{base_url}/checkout/', headers=headers)
             if checkout_response.status_code == 200:
                 print('Contenu de la page Checkout:', checkout_response.text)
